4c9aac795b3aa46446f192a7d6e8b96cc207c714
Sliman Elmansouri <elmansourisliman@gmail.com>
2023-11-24 18:25:14 +0100

    insatllin flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,16 @@
 #!/usr/bin/pup
 # install a specific version of flask
 
-package {'flask':
-    ensure      => '2.1.0',
-    provider    => 'pip3'
-} 
+package { 'python3-pip':
+  ensure => 'installed',
+}
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
