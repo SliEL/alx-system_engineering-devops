ce52df4dbde2880795d0ef7418bea36abdb97831
Sliman Elmansouri <elmansourisliman@gmail.com>
2023-11-24 18:21:25 +0100

    install a specific version of flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
 #!/usr/bin/pup
 # install a specific version of flask
+
 package {'flask':
-    ensure  => '2.1.0',
-    provider => 'pip3',
+    ensure      => '2.1.0',
+    provider    => 'pip3'
 } 
