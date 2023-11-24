250d6778ac79b429f664da184046f6fa9122eff5
Sliman Elmansouri <elmansourisliman@gmail.com>
2023-11-24 17:57:41 +0100

    create a file Puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,6 @@
 file {'/tem/school/':
-    ensure => 'file',
-    mode => '0744',
-    owner => 'www-data',
-    group => 'www-data;,
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data;,
     content => 'I love Puppet\n',
 }
