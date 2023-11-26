caeeea991e81e137cd5de4e4c6b3803f73847820
Sliman Elmansouri <elmansourisliman@gmail.com>
2023-11-26 21:33:59 +0100

    task 4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,16 @@
+file {'/etc/ssh/ssh-ssh_config',
+    ensure  => present,
+
+}
+
 file_line { 'Turn off passwd auth':
   path    => '/etc/ssh/sshd_config',
   line    => 'PasswordAuthentication no',
-  match   => '^#?PasswordAuthentication',
+  match   => '^#PasswordAuthentication',
 }
 
 file_line { 'Declare identity file':
   path    => '/etc/ssh/ssh_config',
-  line    => '    IdentityFile ~/.ssh/school',
-  match   => '^#?IdentityFile',
+  line    => 'IdentityFile ~/.ssh/school',
+  match   => '^#IdentityFile',
 }
