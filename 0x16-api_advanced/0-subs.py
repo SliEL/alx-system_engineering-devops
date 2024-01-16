05bd26b7dca5dc43d4a3cdb5b85a224ff5316bc8
Sliman Elmansouri <elmansourisliman@gmail.com>
2024-01-16 21:28:19 +0100

    requirments fixes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """returns numder of subscripres in a given subreddit."""
-import requests
 from requests import get
 
 
@@ -9,7 +8,6 @@ def number_of_subscribers(subreddit):
     url = f'http://www.reddit.com/r/{subreddit}/about.json'
     header = {"User-Agent": "Mybot"}
     response = get(url, headers=header)
-
     try:
         subreddit_data = response.json()
         return subreddit_data['data']['subscribers']
