e1c5cbf38bb23c9c8dd6444b285d72281d516fe3
Sliman Elmansouri <elmansourisliman@gmail.com>
2024-01-25 13:22:05 +0100

    user limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,20 +1,20 @@
 #increase file open limits for holberton user
 file_line { 'increase_holberton_soft_limit':
+  ensure => present,
   path   => '/etc/security/limits.conf',
   line   => 'holberton soft nofile 4096',
   match  => '^holberton soft nofile',
-  ensure => present,
 }
 
 file_line { 'increase_holberton_hard_limit':
+  ensure => present,
   path   => '/etc/security/limits.conf',
   line   => 'holberton hard nofile 8192',
   match  => '^holberton hard nofile',
-  ensure => present,
 }
 
 file_line { 'ensure_pam_limits':
+  ensure => present,
   path   => '/etc/pam.d/common-session',
   line   => 'session required pam_limits.so',
-  ensure => present,
 }
